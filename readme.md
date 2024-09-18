7bd98f0952cd43717d5f1c266c30972307a62df4
Arion Almond <arionalmond@arions-mbp.lan>
2024-09-18 13:54:13 -0400

    move all random html, js, css examples and testing stuff to one site

---
+++ readme.md
@@ -0,0 +1,32 @@
+# HTMinimaL
+
+scientific method:
+    Question - What - HTML
+    Research - Why - many things need UI
+    Hypothesis - How - Websites, Apps
+    Experiment - Where - Test Site
+    Conclusion? - When - Works!
+    Communicate Results - Who - Teach the previous steps
+
+## Contents
+1. [Overview](#overview)
+2. [Learn/Teach](#learnteach)
+3. [Sources](#sources)
+
+## Overview
+[back to contents](#contents)  
+learning ...  
+teaching ...  
+[back to contents](#contents)  
+
+## Learn/Teach
+
+[back to contents](#contents)  
+learning ...  
+teaching ...  
+[back to contents](#contents)  
+
+## Sources
+[back to contents](#contents)  
+sources ...  
+[back to contents](#contents)  
---
+++ scripts/darkMode.js
@@ -0,0 +1,29 @@
+function dark() {
+    const element = document.documentElement;
+    element.style.backgroundColor = 'black';
+    element.style.color = 'grey';
+};
+
+function grey() {
+    const element = document.documentElement;
+    element.style.backgroundColor = 'grey';
+    element.style.color = 'black';
+};
+
+function bright() {
+    const element = document.documentElement;
+    element.style.backgroundColor = 'white';
+    element.style.color = 'black'
+};
+
+function midBright() {
+    const element = document.documentElement;
+    element.style.backgroundColor = 'whitesmoke';
+    element.style.color = 'grey'
+};
+
+function olive() {
+    const element = document.documentElement;
+    element.style.backgroundColor = 'olive';
+    element.style.color = 'black'
+};
